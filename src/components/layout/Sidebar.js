a9abcf9ab692e5ff518b437d6663ca68afeed4d6
Dubs4Tobi1 <dubs4tobi@gmail.com>
2026-05-30 02:30:05 +0100

    Update Sidebar.js

--- src/components/layout/Sidebar.js
+++ src/components/layout/Sidebar.js
@@ -97,8 +97,8 @@ const Sidebar = ({ isOpen, onClose }) => {
         </nav>
 
         <div className="sidebar-footer">
-          <p>© 2024 StreamVibe</p>
-          <p>University Group Project</p>
+          <p>© 2026 StreamVibe</p>
+          <p>Group L ICT 211</p>
         </div>
       </aside>
     </>
