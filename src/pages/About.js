04a335190ec382af9922e400d82f6073b64c95c6
Dubs4Tobi1 <dubs4tobi@gmail.com>
2026-05-31 16:41:16 +0100

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,21 +5,7 @@ const About = () => {
   const groupMembers = [
     
     { surname: 'Femi-Asoro', otherNames: 'Oluwatobiloba David', matric: 'lcu/ug/24/34240', department: 'Software Engineering' },
-    { surname: 'Tijani', otherNames: 'Farouq', matric: 'LCU/UG/24/32846', department: 'Software Engineering' },
-    { surname: 'Chris', otherNames: 'Samuel Osahon', matric: 'LCU/UG/24/31903', department: 'Software Engineering' },
-    { surname: 'Moka', otherNames: 'Chibuikem Derick', matric: 'LCU/UG/24/31974', department: 'Software Engineering' },
-    { surname: 'Akinsanya', otherNames: 'Emmanuel Iseoluwa', matric: 'LCU/UG/24/30425', department: 'Software Engineering' },
-    { surname: 'Sanni', otherNames: 'Wisdom Arome', matric: 'LCU/UG/24/29436', department: 'Software Engineering' },
-    { surname: 'Ugwuegbu', otherNames: 'Harmony Johnson', matric: 'LCU/UG/24/30322', department: 'Software Engineering' },
-    { surname: 'Anyanwu', otherNames: 'Lincoln Chinwudo', matric: 'LCU/UG/24/32881', department: 'Software Engineering' },
-    { surname: 'Tikare', otherNames: 'Ridwan Adeola', matric: 'Lcu/Ug/25/38690', department: 'Software Engineering' },
-    { surname: 'Omoriba', otherNames: 'Oluwafeyisanmi Paul', matric: 'LCU/UG/24/31710', department: 'Software Engineering' },
-    { surname: 'Ayodele', otherNames: 'Olanrewaju James', matric: 'LCU/UG/24/33599', department: 'Software Engineering' },
-    { surname: 'Umah', otherNames: 'Justice ugochukwu', matric: 'LCU/UG/24/30299', department: 'Software Engineering' },
-    { surname: 'Atipo', otherNames: 'Amazinggrace Ayomide', matric: 'Lcu/Ug/24/29304', department: 'Software Engineering' },
-    { surname: 'Kalejaiye', otherNames: 'Oluwatumininu Ayomikun', matric: 'LCU/UG/24/32204', department: 'Software Engineering' },
-    { surname: 'Kassim-Ashiru', otherNames: 'Kassim Olansile', matric: 'LCU/UG/25/39600', department: 'Software Engineering' },
-    
+
   ];
 
   return (
